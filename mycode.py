b92f3d5524085ff1e9b6f1eabe661133da293967
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-11-11 20:00:10 +0000

    Nov 11, 2023, 8:00 PM

--- mycode.py
+++ mycode.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 
-# Read data from file
 with open('metrisis.txt', 'r') as f:
     data = f.readlines()
 
-# Split data into x and y values
 x = []
 y = []
 for line in data:
@@ -12,7 +10,6 @@ for line in data:
     x.append(float(values[0]))
     y.append(float(values[1]))
 
-# Create bar chart
 plt.bar(x, y, color='blue',width = 0.088, edgecolor='black')
 plt.xlabel('X Axis')
 plt.ylabel('Y Axis')
