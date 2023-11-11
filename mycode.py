373feb422a73bdd181e046d838f19ee4b3eceaaf
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-11-11 19:48:28 +0000

    Nov 11, 2023, 7:48 PM

--- mycode.py
+++ mycode.py
@@ -12,11 +12,25 @@ for line in data:
     x.append(float(values[0]))
     y.append(float(values[1]))
 
+# Create a new figure with a specific size (width, height)
+plt.figure(figsize=(10, 6))
+
 # Create bar chart
 plt.bar(x, y, color='blue', width=0.1)
-plt.xlabel('X Axis')
-plt.ylabel('Y Axis')
-plt.title('Bar Chart')
-plt.savefig('barchart.png')
+
+# Add grid lines
+plt.grid(True)
+
+# Set labels and title with a larger font size
+plt.xlabel('X Axis', fontsize=14)
+plt.ylabel('Y Axis', fontsize=14)
+plt.title('Bar Chart', fontsize=16)
+
+# Rotate x-axis labels if they overlap
+plt.xticks(rotation=45)
+
+# Save the figure
+plt.savefig('barchart.png', bbox_inches='tight')
+
 print('Done')
 quit()
