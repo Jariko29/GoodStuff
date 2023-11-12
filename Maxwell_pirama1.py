163265f2b04e9e398cb5f5f78b92572cf2f31caa
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-11-12 20:13:28 +0000

    Nov 12, 2023, 8:13 PM

--- Maxwell_pirama1.py
+++ Maxwell_pirama1.py
@@ -3,18 +3,14 @@ from scipy.stats import maxwell
 
 with open('metrisis.txt', 'r') as f:
     data = f.readlines()
-
 x = []
 y = []
 for line in data:
     values = line.split()
     x.append(float(values[0]))
     y.append(float(values[1]))
-
 params = maxwell.fit(y, floc=0)
-print(params)
 R = maxwell.pdf(x, *params)  
-print(R)
 
 plt.bar(x, y, color='blue',width = 0.088, edgecolor='black')
 plt.plot(x, R, color='red', linewidth=1)
