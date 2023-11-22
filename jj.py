4ccc1c623ba2cd4b2ae3e9b6e765975514cbe237
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-11-22 19:05:43 +0000

    Nov 22, 2023, 7:05 PM

--- jj.py
+++ jj.py
@@ -1,16 +1,36 @@
 import math
+import cmath
 
-# Δεδομένα
-Vm = 150  # Μέγιστη τάση
-R = 40.0  # Αντίσταση
-L = 80.0e-3  # Πηνίο αυτεπαγωγής
-C = 50.0e-6  # Πυκνωτής
-frequency = 100  # Συχνότητα
+# Given data
+Vm = 150  # Maximum voltage
+R = 40.0  # Resistance
+L = 80.0e-3  # Inductance
+C = 50.0e-6  # Capacitance
+
+
+# Angular frequency
+omega = 100
+
+# Impedances
+Z_R = R
+Z_L = 1j * omega * L
+Z_C = 1 / (1j * omega * C)
+
+# Total impedance
+Z_total = Z_R + Z_L + Z_C
+
+# Current in the circuit
+I = Vm / abs(Z_total)
+
+# Voltages across the resistor, inductor, and capacitor
+VR0 = I * abs(Z_R)
+VL0 = I * abs(Z_L)
+VC0 = I * abs(Z_C)
 
 # Represent the voltages as complex numbers
 VR0_complex = VR0
-VL0_complex = VL0 * (1j)  # The voltage across the inductor leads by 90 degrees
-VC0_complex = VC0 * (-1j)  # The voltage across the capacitor lags by 90 degrees
+VL0_complex = VL0 * cmath.exp(1j * cmath.phase(Z_L))  # The voltage across the inductor leads by 90 degrees
+VC0_complex = VC0 * cmath.exp(1j * cmath.phase(Z_C))  # The voltage across the capacitor lags by 90 degrees
 
 # Add the complex voltages
 V_total_complex = VR0_complex + VL0_complex + VC0_complex
@@ -20,31 +40,6 @@ V_total = abs(V_total_complex)
 
 # Now you can check if V_total is close to Vm
 if math.isclose(V_total, Vm, rel_tol=1e-9):
-    print("Οι τάσεις είναι περίπου ίσες.")
+    print("The voltages are approximately equal.")
 else:
-    print("Οι τάσεις δεν είναι ίσες.")
-
-
-# Υπολογισμοί
-Im = Vm / (R * math.sqrt(2))  # Ρευματική ένταση
-omega = 2 * math.pi * frequency  # Γωνιακή συχνότητα
-
-# Πτώση τάσης στην αντίσταση (VR0)
-VR0 = Im * R
-
-# Πτώση τάσης στο πηνίο (VL0)
-VL0 = Im * omega * L
-
-# Πτώση τάσης στον πυκνωτή (VC0)
-VC0 = Im / (omega * C)
-
-# Ελέγχουμε εάν η συνολική τάση είναι περίπου ίση με την αρχική τάση
-if math.isclose(VR0 + VL0 + VC0, Vm, rel_tol=1e-9):
-    print("Οι τάσεις είναι περίπου ίσες.")
-else:
-    print("Οι τάσεις δεν είναι ίσες.")
-
-# Εκτύπωση αποτελεσμάτων
-print(f"VR0: {VR0} V")
-print(f"VL0: {VL0} V")
-print(f"VC0: {VC0} V")
+    print("The voltages are not equal.")
