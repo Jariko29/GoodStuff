f3323bd2e30bc81651fedefd673608eb6df18da8
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-11-22 19:04:18 +0000

    Nov 22, 2023, 7:04 PM

--- jj.py
+++ jj.py
@@ -7,6 +7,24 @@ L = 80.0e-3  # Πηνίο αυτεπαγωγής
 C = 50.0e-6  # Πυκνωτής
 frequency = 100  # Συχνότητα
 
+# Represent the voltages as complex numbers
+VR0_complex = VR0
+VL0_complex = VL0 * (1j)  # The voltage across the inductor leads by 90 degrees
+VC0_complex = VC0 * (-1j)  # The voltage across the capacitor lags by 90 degrees
+
+# Add the complex voltages
+V_total_complex = VR0_complex + VL0_complex + VC0_complex
+
+# The magnitude of the total voltage is the absolute value of the complex total voltage
+V_total = abs(V_total_complex)
+
+# Now you can check if V_total is close to Vm
+if math.isclose(V_total, Vm, rel_tol=1e-9):
+    print("Οι τάσεις είναι περίπου ίσες.")
+else:
+    print("Οι τάσεις δεν είναι ίσες.")
+
+
 # Υπολογισμοί
 Im = Vm / (R * math.sqrt(2))  # Ρευματική ένταση
 omega = 2 * math.pi * frequency  # Γωνιακή συχνότητα
