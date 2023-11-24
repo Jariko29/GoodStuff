6496efa23da9ecf677c6391fd2acf9b2df7562a0
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-11-24 20:34:16 +0000

    Nov 24, 2023, 8:34 PM

--- jj.py
+++ jj.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Circuit parameters
-C = 1.0  # Capacitance (in Farads)
-L = 1.0  # Inductance (in Henrys)
+C = 10.0  # Capacitance (in Farads)
+L = 10.0  # Inductance (in Henrys)
 R = 1.0  # Resistance (in Ohms)
 
 # Time range
-t = np.linspace(0, 10, 1000)  # Time values from 0 to 10 seconds
+t = np.linspace(0, 120, 1000)  # Time values from 0 to 10 seconds
 
 # Charge (Q) and current (I) calculations
 Q = np.exp(-R * t / (2 * L)) * np.cos(np.sqrt(1 / (L * C) - (R / (2 * L))**2) * t)
@@ -20,5 +20,5 @@ plt.xlabel('Time (s)')
 plt.ylabel('Charge (C) / Current (A)')
 plt.legend()
 plt.grid(True)
-plt.show()
+plt.savefig('RLC.png')
 quit()
