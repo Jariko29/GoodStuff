ec2d33a1386e95f0f4a40662b09c5c099cc52cdd
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-11-24 19:11:03 +0000

    Nov 24, 2023, 7:11 PM

--- jj.py
+++ jj.py
@@ -1,54 +1,27 @@
 import math
-import cmath
 
-# Given data
-Vm = 150  # Maximum voltage
-R = 40.0  # Resistance
-L = 80.0e-3  # Inductance
-C = 50.0e-6  # Capacitance
+# Δεδομένα
+R = 1.0  # Εσωτερική αντίσταση του πηνίου σε ohms
+L = 400e-3  # Αυτεπαγωγή του πηνίου σε henrys
+C = 2.0e-6  # Χωνευτικότητα του πυκνωτή σε farads
+V0 = 24.0  # Αρχική τάση στο πηνίο σε volts
 
+# (α) Αρχικά τάση στο πηνίο
+V_initial = V0
 
-# Angular frequency
-omega = 100
+# (β) Ενέργεια που χάνεται
+omega = math.sqrt(1 / (L * C) - R**2 / (4 * L**2))
+I = V0 / math.sqrt(L**2 * (1 / (L * C) - R**2 / (4 * L**2)))
+E_lost = 0.5 * L * I**2
 
-# Impedances
-Z_R = R
-Z_L = 1j * omega * L
-Z_C = 1 / (1j * omega * C)
+# (γ) Συχνότητα των ταλαντώσεων
+frequency = omega / (2 * math.pi)
 
-# Total impedance
-Z_total = Z_R + Z_L + Z_C
+# (δ) Παράγοντας ποιότητας
+Q_factor = math.sqrt(L / C) / R
 
-print("Total impedance:", Z_total)
-
-# Current in the circuit
-I = Vm / abs(Z_total)
-
-print("Current in the circuit:", I)
-
-# Voltages across the resistor, inductor, and capacitor
-VR0 = I * abs(Z_R)
-VL0 = I * abs(Z_L)
-VC0 = I * abs(Z_C)
-
-# Represent the voltages as complex numbers
-VR0_complex = VR0
-VL0_complex = VL0 * cmath.exp(1j * cmath.phase(Z_L))  # The voltage across the inductor leads by 90 degrees
-VC0_complex = VC0 * cmath.exp(1j * cmath.phase(Z_C))  # The voltage across the capacitor lags by 90 degrees
-
-# Add the complex voltages
-V_total_complex = VR0_complex + VL0_complex + VC0_complex
-
-# The magnitude of the total voltage is the absolute value of the complex total voltage
-V_total = abs(V_total_complex)
-
-# Now you can check if V_total is close to Vm
-if math.isclose(V_total, Vm, rel_tol=1e-9):
-    print("The voltages are approximately equal.")
-else:
-    print("The voltages are not equal.")
-    
-print("Voltage across the resistor (VR0):", VR0)
-print("Voltage across the inductor (VL0):", VL0)
-print("Voltage across the capacitor (VC0):", VC0)
-print("Total voltage (V_total):", V_total)
+# Εκτύπωση αποτελεσμάτων
+print("(α) Αρχικά τάση στο πηνίο:", V_initial, "V")
+print("(β) Ενέργεια που χάνεται:", E_lost, "Joules")
+print("(γ) Συχνότητα των ταλαντώσεων:", frequency, "Hz")
+print("(δ) Παράγοντας ποιότητας:", Q_factor)
