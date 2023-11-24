40d7dc1ab183c43885e5ca2e24178f856e44562d
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-11-24 20:56:03 +0000

    Nov 24, 2023, 8:56 PM

--- jj.py
+++ jj.py
@@ -7,19 +7,20 @@ L = 10.0  # Inductance (in Henrys)
 R = 1.0  # Resistance (in Ohms)
 
 # Time range
-t = np.linspace(0, 120, 1000)  # Time values from 0 to 10 seconds
+t = np.linspace(0, 120, 1000)  # Time values from 0 to 120 seconds
 
 # Charge (Q) and current (I) calculations
 Q = np.exp(-R * t / (2 * L)) * np.cos(np.sqrt(1 / (L * C) - (R / (2 * L))**2) * t)
 I = -np.sqrt(1 / (L * C) - (R / (2 * L))**2) * np.exp(-R * t / (2 * L)) * np.sin(np.sqrt(1 / (L * C) - (R / (2 * L))**2) * t)
 
 # Plotting Q(t) and I(t)
-plt.figure()
+plt.figure(figsize=(10, 6))
 plt.plot(t, Q, label='Q(t)')
 plt.plot(t, I, label='I(t)')
 plt.xlabel('Time (s)')
 plt.ylabel('Charge (C) / Current (A)')
+plt.title('Charge and Current vs Time')
 plt.legend()
 plt.grid(True)
-plt.savefig('RLC.png')
-quit()
+plt.show()
+plt.savefig('barchart.png')
