e8530a988116d659c63f10462efd0dfcafb0b228
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-11-24 20:57:41 +0000

    Nov 24, 2023, 8:57 PM

--- jj.py
+++ jj.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 # Circuit parameters
 C = 10.0  # Capacitance (in Farads)
 L = 10.0  # Inductance (in Henrys)
-R = 1.0  # Resistance (in Ohms)
+R = 0.0  # Resistance (in Ohms)
 
 # Time range
 t = np.linspace(0, 120, 1000)  # Time values from 0 to 120 seconds
 
 # Charge (Q) and current (I) calculations
-Q = np.exp(-R * t / (2 * L)) * np.cos(np.sqrt(1 / (L * C) - (R / (2 * L))**2) * t)
-I = -np.sqrt(1 / (L * C) - (R / (2 * L))**2) * np.exp(-R * t / (2 * L)) * np.sin(np.sqrt(1 / (L * C) - (R / (2 * L))**2) * t)
+Q = np.cos(np.sqrt(1 / (L * C)) * t)
+I = -np.sqrt(1 / (L * C)) * np.sin(np.sqrt(1 / (L * C)) * t)
 
 # Plotting Q(t) and I(t)
 plt.figure(figsize=(10, 6))
