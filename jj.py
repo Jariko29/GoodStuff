bd33c09f9adb16d8d7a68477baa7333db21a2dbe
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-11-29 16:34:22 +0000

    Nov 29, 2023, 4:34 PM

--- jj.py
+++ jj.py
@@ -1,22 +1,22 @@
 from sympy import symbols, Eq, solve, cos, sin, rad
 
-# Σύμβολα
+# Symbols
 R, Im, phi = symbols('R Im phi')
 
-# Δεδομένα
+# Data
 omega = 200  # rad/s
 Vin_amplitude = 20
 Vin_phase = 30  # degrees
 L = 0.4  # H
 
-# Εξίσωση για τον όρο που περιέχει την αντίσταση
-eq1 = Eq(Im * R, Vin_amplitude * cos(Vin_phase * rad))
+# Equation for the term containing the resistance
+eq1 = Eq(Im * R, Vin_amplitude * cos(rad(Vin_phase)))
 
-# Εξίσωση για τον όρο που περιέχει τον όρο του πηνίου
-eq2 = Eq(Im * L * omega, Vin_amplitude * sin(Vin_phase * rad))
+# Equation for the term containing the inductor term
+eq2 = Eq(Im * L * omega, Vin_amplitude * sin(rad(Vin_phase)))
 
-# Λύση του συστήματος εξισώσεων
-solution = solve((eq1, eq2), (R, Im, phi))
+# Solve the system of equations
+solution = solve((eq1, eq2), (R, Im))
 
-# Εκτύπωση της απάντησης
-print(f"The value of R is: {solution[R]:.2f} ohms")
+# Print the solution
+print(solution)
