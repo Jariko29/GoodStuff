7babf638d6dfeab865f8eff00d2cb32458c5c7a0
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2023-12-01 21:28:43 +0000

    Dec 1, 2023, 9:28 PM

--- Maxwell_pirama1.py
+++ Maxwell_pirama1.py
@@ -1,22 +1,24 @@
-import matplotlib.pyplot as plt
-from scipy.stats import maxwell
+import math
 
-with open('metrisis.txt', 'r') as f:
-    data = f.readlines()
-x = []
-y = []
-for line in data:
-    values = line.split()
-    x.append(float(values[0]))
-    y.append(float(values[1]))
-params = maxwell.fit(y, floc=0)
-R = maxwell.pdf(x, *params)  
+def calculate_new_beam_diameter(initial_diameter, wavelength, distance):
+    # Convert initial diameter to meters
+    initial_diameter_meters = initial_diameter / 1000.0  # converting mm to meters
 
-plt.bar(x, y, color='blue',width = 0.088, edgecolor='black')
-plt.plot(x, R, color='red', linewidth=1)
-plt.xlabel('Ταχύτητα (m/s)')
-plt.ylabel('Συχνότητα (μπάλες/s)')
-plt.title('Ιστόγραμμα Κατανομής Ταχυτήτων-Maxwell')
-plt.savefig('barchart.png')
-print('Done')
-quit()
+    # Calculate diffraction-limited beam diameter
+    diffraction_limited_diameter = (4 * wavelength * distance) / (math.pi * initial_diameter_meters)
+
+    # Calculate angular spread
+    theta = diffraction_limited_diameter / distance
+
+    # Calculate new beam diameter at the given distance
+    new_beam_diameter = 2 * distance * math.tan(theta / 2)
+
+    return new_beam_diameter
+
+# Example usage:
+initial_beam_diameter = 0.5  # in mm
+wavelength = 0.633 * 1e-6  # wavelength for a helium-neon laser in meters
+distance = 400000.0  # in meters
+
+new_beam_diameter = calculate_new_beam_diameter(initial_beam_diameter, wavelength, distance)
+print(f"New beam diameter at {distance} meters: {new_beam_diameter} meters")
