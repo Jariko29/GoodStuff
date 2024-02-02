aed0d96095f479da46fc6e0f1ffff1f7048a6c79
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2024-02-02 22:21:59 +0000

    Feb 2, 2024, 10:21 PM

--- analisi.py
+++ analisi.py
@@ -27,7 +27,7 @@ def plot_graph(x, y, slope, ordinate, yerr):
     plt.scatter(x, y)
     plt.plot(x, [slope*x_i + ordinate for x_i in x])
     plt.xlim(left=0)
-    plt.title('1/y_m = f(1/m^2)')
+    plt.title(0.5,0.95,r'$\frac{1}{{y_m}^2} = f(\frac{1}{{m}^2})$', fontsize=12)
     equation = f"y = {slope:.2f}x  {ordinate:.2f}"
     plt.text(0.89, 0.15, equation, transform=plt.gca().transAxes, fontsize=10, verticalalignment='bottom',horizontalalignment='right')
     plt.savefig('analisi.png', dpi=300)
