ff4e283d2848654ec514a160ae2b32af7020e48a
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2024-02-22 20:47:01 +0000

    Feb 22, 2024, 8:47 PM

--- 2024uni/Lab04/aanalisi.py
+++ 2024uni/Lab04/aanalisi.py
@@ -61,7 +61,7 @@ def plot_graph(x, y,plotname,fig):# slope, ordinate, yerr,
     #---creating fit functions---
     #----------------------------
     def func (x,a,b):
-       return a*(np.tan(np.radians(x-b)))**2/(np.tan(np.radians(x+b)))**2 # fit , change if something else
+       return a*(np.sin(np.radians(x-b)))**2/(np.sin(np.radians(x+b)))**2 # fit , change if something else
     n=len(x)
     params,params_covariance=curve_fit(func,x,y,p0=[2,51*np.pi/180])
     print("parametri:",params) 
