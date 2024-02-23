db3118541d868967900d5c85180ab9008e397147
Jariko29 <138761593+Jariko29@users.noreply.github.com>
2024-02-23 09:04:32 +0000

    Feb 23, 2024, 9:04 AM

--- 2024uni/Lab04/aanalisi.py
+++ 2024uni/Lab04/aanalisi.py
@@ -97,5 +97,5 @@ print(n2a,'\t',n2b  )
 #------------plot graph--------------------------
 #------------------------------------------------
 plot_graph(thetar1,ratio1,'graph1',1,func1,[0,np.pi/6])
-plot_graph(thetar2,ratio2,'graph2',2,func2,[1,0.5])
+plot_graph(thetar2,ratio2,'graph2',2,func2,[0,0.5])
 plot_graph(thetar3,ratio3,'graph3',3,func1)
